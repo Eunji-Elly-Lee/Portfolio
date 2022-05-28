14b2a668b1bd0769d23ba2ab4cdebb0554dd5464
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-28 15:37:22 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <div>
