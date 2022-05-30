65143189d4308e2613b8fbcf50acf7fe9c94acc1
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-30 00:25:27 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,35 @@
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import snapshot from '../src_assets/snapshot.jpg';
+
 function About() {
   return (
-    null
+    <section id="about">
+      <Container>
+        <Row>
+          <Col md className="profile">
+            <h1>
+              Hi, I'm Eunji Elly Lee.
+              <br />
+              I love to write code!
+            </h1>
+            <p>
+              I'm a new graduate who successfully completed the Software Development major
+              in the Information Technology program at SAIT.
+              I truly enjoy writing code and developing websites and web applications
+              using Java, JavaScript, Python, React, and SQL.
+              Also, making applications awesome using Bootstrap and CSS is so much fun for me.
+            </p>
+            <div>
+              <Button className="button" variant="success" href="#contact">Work With Me</Button>
+              <Button className="button" variant="outline-success" href="#projects">See My Projects</Button>
+            </div>
+          </Col>
+          <Col md>
+            <img className="snap" alt="snapshot" src={snapshot} />
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
 
