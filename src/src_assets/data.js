496d09ffa797c7de6b3cf0624cd7827e900666f6
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-31 00:31:17 -0600

    Create data.js

---
+++ src/src_assets/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Blog",
+    subtitle: "Jekyll / Markdown",
+    description:
+      "Posted basics and core concepts of Java and Python",
+    image: "../src_assets/blog.png",
+    link: "https://eunji-elly-lee.github.io/",
+  },
+  {
+    title: "Inventory Management",
+    subtitle: "JSP / Servlet / MySQL / Apache tomcat",
+    description:
+      "Implemented home item management system including user data management",
+    image: "../src_assets/inventory.png",
+    link: "",
+  },
+  {
+    title: "Searching Dataset",
+    subtitle: "JavaScript / AJAX",
+    description:
+      "Provided the search function for data about city operations published by the city of Calgary",
+    image: "../src_assets/searching.png",
+    link: "",
+  },
+  {
+    title: "Medical Appointment",
+    subtitle: "JSP / Servlet / MySQL / Apache tomcat / AWS",
+    description:
+      "Implemented appointment booking and management system including patients and staff data management",
+    image: "../src_assets/medical.png",
+    link: "",
+  },
+];
