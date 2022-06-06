c1c3ff1e92a8fc68cc8ebecb8718bcc684c25b21
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-06 02:40:03 -0600

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,38 @@
+import { Container, Row } from 'react-bootstrap';
+import { MdWeb } from "react-icons/md";
+import { projects } from "../data";
+import './Projects.css';
+import Project from "./Project"
+
 function Projects() {
   return (
-    null
+    <section id="projects">
+      <div>
+        <MdWeb />
+        <h1>
+          My projects
+        </h1>
+        <p>
+          I am dedicated to self-development and professional growth and
+          have continuously worked on projects in GitHub using various languages.
+        </p>
+      </div>
+      <Container> 
+        <Row>      
+          {projects.map((project) => (
+            <Project 
+              key={project.title}
+              image={project.image}
+              subtitle={project.subtitle}
+              title={project.title}
+              description={project.description}
+              link={project.link}
+              code={project.code}
+            />
+          ))}    
+          </Row>      
+        </Container>
+    </section>
   );
 }
 
