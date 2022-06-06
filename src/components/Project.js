01cd3cae189b75744216c77bb1c4bb053684c388
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-06 02:45:22 -0600

    Create Project.js

---
+++ src/components/Project.js
@@ -0,0 +1,30 @@
+import { Col, Button } from 'react-bootstrap';
+import PropTypes from "prop-types";
+
+function Project({image, subtitle, title, description, link, code}) {
+  return (
+    <Col md="6">
+      <img alt={title} src={image} />
+      <div>
+        <h3>{subtitle}</h3>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div>
+          <Button href={link} target="_blank">View</Button>
+          <Button href={code} target="_blank">Code</Button>
+        </div>
+      </div>
+    </Col>
+  );
+}
+
+Project.propTypes = {
+  image: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  describe: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  code: PropTypes.string.isRequired,
+}
+
+export default Project;
