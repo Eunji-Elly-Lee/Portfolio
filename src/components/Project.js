4857ce11e881314633a5d07a3fbdf40a0cd0d97d
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-07 00:48:56 -0600

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,16 +3,22 @@ import PropTypes from "prop-types";
 
 function Project({image, subtitle, title, description, link, code}) {
   return (
-    <Col md="6">
-      <img alt={title} src={image} />
-      <div>
-        <h3>{subtitle}</h3>
+    <Col lg="5" className="project m-2 p-0">
+      <div className="description">
+        <h5>{subtitle}</h5>
         <h2>{title}</h2>
         <p>{description}</p>
         <div>
-          <Button href={link} target="_blank">View</Button>
-          <Button href={code} target="_blank">Code</Button>
+          <Button className="me-1 mx-sm-1" variant="success" href={link} target="_blank">
+            View
+          </Button>
+          <Button className="mx-sm-1" variant="outline-success" href={code} target="_blank">
+            Code
+          </Button>
         </div>
+      </div>
+      <div className="image-wrap">
+        <img alt={title} src={image} />
       </div>
     </Col>
   );
