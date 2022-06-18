8f8239b268424afc921ce89e80e172ece0400cb2
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-17 22:42:41 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,38 +1,38 @@
 export const projects = [
   {
     title: "Blog",
-    subtitle: "Jekyll / Markdown",
+    subtitle: "Jekyll / Markdown / Disqus / Google Analytics",
     description:
-      "Posted basics and core concepts of Java and Python and my self-introduction",
+      "Created blog posts on core concepts of Java and Python including implementation of a Python game",
     image: "./assets/blog.png",
     link: "https://eunji-elly-lee.github.io/",
     code: "https://github.com/Eunji-Elly-Lee/eunji-elly-lee.github.io",
   },
   {
     title: "Inventory Management",
-    subtitle: "JSP / Servlet / MySQL / Tomcat / AWS",
+    subtitle: "JSP / Servlet / MySQL / Apache Tomcat / AWS",
     description:
-      "Implemented home item management system including user data management",
+      "Implemented home item management system including user data management functionalities",
     image: "./assets/inventory.png",
     link: "http://3.96.237.87:8080/Inventory-Management/",
     code: "https://github.com/Eunji-Elly-Lee/Inventory-Management",
   },
   {
-    title: "Searching Dataset",
-    subtitle: "JavaScript / AJAX / Netlify",
-    description:
-      "Provided the search format pages for data about city operations published by the city of Calgary",
-    image: "./assets/searching.png",
-    link: "https://elly-search-dataset.netlify.app/",
-    code: "https://github.com/Eunji-Elly-Lee/Search-Dataset",
-  },
-  {
     title: "Medical Appointment",
-    subtitle: "JSP / Servlet / MySQL / Tomcat / AWS",
+    subtitle: "JSP / Servlet / MySQL / Apache Tomcat / AWS",
     description:
-      "Implemented appointment booking and management system including patients and staff data management",
+      "Implemented appointment booking and management system including patients and staff data management functionalities",
     image: "./assets/medical.png",
     link: "http://3.96.237.87:8080/Medical-Appointment/",
     code: "https://github.com/Eunji-Elly-Lee/Medical-Appointment",
+  },
+  {
+    title: "Searching Dataset",
+    subtitle: "JavaScript / AJAX / Dataset JSON / Netlify",
+    description:
+      "Developed the search page user interface for searching data about city operations published by the city of Calgary",
+    image: "./assets/searching.png",
+    link: "https://elly-search-dataset.netlify.app/",
+    code: "https://github.com/Eunji-Elly-Lee/Search-Dataset",
   },
 ];
