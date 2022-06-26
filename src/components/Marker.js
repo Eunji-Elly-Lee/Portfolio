53cdf6601a7f06547507ed9a72c36d3cfabeb1f5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-26 00:05:05 -0600

    Create Marker.js

---
+++ src/components/Marker.js
@@ -0,0 +1,9 @@
+import { GiPositionMarker } from "react-icons/gi";
+
+function Marker() {
+  return (
+    <GiPositionMarker style={{color: "red", fontSize: "2em"}}/>
+  );
+}
+
+export default Marker;
