2622b3dad23b8eafa844b1b4612beb6855caf21d
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-09-08 12:07:15 -0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const projects = [
   {
-    title: "Blog",
-    subtitle: "Jekyll / Markdown / Disqus / Google Analytics",
+    title: "Food Ordering",
+    subtitle: "React / Node / Express / Stripe / Firebase / Netlify",
     description:
-      "Created blog posts on core concepts of Java and Python including implementation of a Python game",
-    image: "./assets/blog.png",
-    link: "https://eunji-elly-lee.github.io/",
-    code: "https://github.com/Eunji-Elly-Lee/eunji-elly-lee.github.io",
+      "Implemented food ordering application including payment system, menu management, and order histories",
+    image: "./assets/food.png",
+    link: "https://elly-gimbap.netlify.app/",
+    code: "https://github.com/Eunji-Elly-Lee/Gimbap",
   },
   {
-    title: "Inventory Management",
-    subtitle: "JSP / Servlet / MySQL / Apache Tomcat / AWS",
+    title: "Searching Dataset",
+    subtitle: "JavaScript / AJAX / Dataset JSON / Netlify",
     description:
-      "Implemented home item management system including user data management functionalities",
-    image: "./assets/inventory.png",
-    link: "http://3.96.237.87:8080/Inventory-Management/",
-    code: "https://github.com/Eunji-Elly-Lee/Inventory-Management",
+      "Developed the search page user interface for searching data about city operations published by the city of Calgary",
+    image: "./assets/searching.png",
+    link: "https://elly-search-dataset.netlify.app/",
+    code: "https://github.com/Eunji-Elly-Lee/Search-Dataset",
   },
   {
     title: "Medical Appointment",
@@ -27,12 +27,21 @@ export const projects = [
     code: "https://github.com/Eunji-Elly-Lee/Medical-Appointment",
   },
   {
-    title: "Searching Dataset",
-    subtitle: "JavaScript / AJAX / Dataset JSON / Netlify",
+    title: "Inventory Management",
+    subtitle: "JSP / Servlet / MySQL / Apache Tomcat / AWS",
     description:
-      "Developed the search page user interface for searching data about city operations published by the city of Calgary",
-    image: "./assets/searching.png",
-    link: "https://elly-search-dataset.netlify.app/",
-    code: "https://github.com/Eunji-Elly-Lee/Search-Dataset",
+      "Implemented home item management system including user data management functionalities",
+    image: "./assets/inventory.png",
+    link: "http://3.96.237.87:8080/Inventory-Management/",
+    code: "https://github.com/Eunji-Elly-Lee/Inventory-Management",
   },
+  {
+    title: "Blog",
+    subtitle: "Jekyll / Markdown / Disqus / Google Analytics",
+    description:
+      "Created blog posts on basics and core concepts of Java and Python including how to instructions for the implementation of a Python game",
+    image: "./assets/blog.png",
+    link: "https://eunji-elly-lee.github.io/",
+    code: "https://github.com/Eunji-Elly-Lee/eunji-elly-lee.github.io",
+  }
 ];
