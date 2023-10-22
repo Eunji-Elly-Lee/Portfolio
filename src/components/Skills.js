292d01423b311662dc493512189f123c9db42a2a
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-10-21 21:24:31 -0600

    Update skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -30,109 +30,109 @@ function Skills() {
       </div>
       <Container>
         <Row className="justify-content-center">
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaNode className="icon mb-2 mb-sm-2 me-2" />
               Node.js
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaReact className="icon mb-2 mb-sm-2 me-2" />
               React
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiNextdotjs className="icon mb-2 mb-sm-2 me-2" />
               Next.js
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiTypescript className="icon mb-2 mb-sm-2 me-2" />
               Typescript
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiRedux className="icon mb-2 mb-sm-2 me-2" />
               Redux
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaJs className="icon mb-2 mb-sm-2 me-2" />
               JavaScript
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaJava className="icon mb-2 mb-sm-2 me-2" />
               Java
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaPython className="icon mb-2 mb-sm-2 me-2" />
               Python
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiOracle className="icon mb-2 mb-sm-2 me-2" />
               Oracle
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiMysql className="icon mb-2 mb-sm-2 me-2" />
               MySQL
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiMongodb className="icon mb-2 mb-sm-2 me-2" />
               MongoDB
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaAws className="icon mb-2 mb-sm-2 me-2" />
               AWS
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiNetlify className="icon mb-2 mb-sm-2 me-2" />
               Netlify
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiFirebase className="icon mb-2 mb-sm-2 me-2" />
               Firebase
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaGithub className="icon mb-2 mb-sm-2 me-2" />
               GitHub
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiJekyll className="icon mb-2 mb-sm-2 me-2" />
               Jekyll
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <SiDisqus className="icon mb-2 mb-sm-2 me-2" />
               Disqus
             </h2>
           </Col>
-          <Col xs="5" lg="3" className="bg-dark m-2 pt-3">
+          <Col xs="5" lg="3" className="bg-dark m-2 pt-3 rounded-2">
             <h2>
               <FaStripe className="icon mb-2 mb-sm-2 me-2" />
               Stripe
