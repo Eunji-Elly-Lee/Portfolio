2a034a772f39ad03f1e7e1eaa5840d0bfc481a09
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-04-06 22:12:14 -0600

    Update project data set

--- src/data.js
+++ src/data.js
@@ -1,50 +1,66 @@
 export const projects = [
   {
     title: "Restaurant Reviews",
-    subtitle: "React / Node / Express / MongoDB",
+    subtitle: "React / Node / Express / Bootstrap /MongoDB",
     description:
-      "An application for reviewing restaurants with search functionality and user authentication",
+      "Reviewing restaurants system with search functionality and user authentication",
     image: "./assets/restaurants.png",
     code: "https://github.com/Eunji-Elly-Lee/Restaurant-Reviews",
   },
   {
-    title: "Food Ordering",
-    subtitle: "React / Node / Express / Stripe / Firebase",
+    title: "Kimbap Ordering",
+    subtitle: "React / Node / Express / Bootstrap / Firebase / Stripe",
     description:
-      "An application for ordering food with features such as payment system, menu management, and order tracker",
+      "Online Kimbap ordering system with a secure and convenient payment gateway using Stripe",
     image: "./assets/food.png",
     code: "https://github.com/Eunji-Elly-Lee/Gimbap",
   },
   {
-    title: "Searching Dataset",
-    subtitle: "JavaScript / AJAX / Dataset JSON",
+    title: "Medical Appointment",
+    subtitle: "Java Servlet / JSP / JavaMail / MySQL",
     description:
-      "A search page interface for querying city operations data, sourced from the city of Calgary",
-    image: "./assets/searching.png",
-    code: "https://github.com/Eunji-Elly-Lee/Search-Dataset",
-  },
-  {
-    title: "Medical Clinic Appointment",
-    subtitle: "JSP / Java / MySQL / Apache Tomcat",
-    description:
-      "An application for medical clinic with features such as online booking and management system for patient and staff data",
+      "Clinic management system featuring online appointment booking and secure staff data management",
     image: "./assets/medical.png",
     code: "https://github.com/Eunji-Elly-Lee/Medical-Appointment",
   },
   {
-    title: "Inventory Management",
-    subtitle: "JSP / Servlet / MySQL / Apache Tomcat / AWS",
+    title: "Pang",
+    subtitle: "Python",
     description:
-      "An application for home item management system including user data management functionalities",
+      "Pang - Arcade Video Game in Python",
+    image: "./assets/pang.png",
+    code: "https://github.com/Eunji-Elly-Lee/Pang-In-Python",
+  },
+  {
+    title: "Search Open Dataset",
+    subtitle: "JavaScript / AJAX / JSON Parsing / Netlify",
+    description:
+      "Search page interface for querying city operations data, sourced from the city of Calgary",
+    image: "./assets/searching.png",
+    code: "https://github.com/Eunji-Elly-Lee/Search-Dataset",
+  },
+  {
+    title: "Inventory Management",
+    subtitle: "Java Servlet / JSP / JavaMail / Bootstrap / MySQL",
+    description:
+      "Role-based inventory management system through secure access control and user-specific functionalities",
     image: "./assets/inventory.png",
     code: "https://github.com/Eunji-Elly-Lee/Inventory-Management",
   },
   {
-    title: "Blog",
-    subtitle: "Jekyll / Markdown / Disqus / Google Analytics",
+    title: "Hangman",
+    subtitle: "Java",
     description:
-      "Blog posts on basics and core concepts of Java and Python including how to instructions for the implementation of a Python game",
-    image: "./assets/blog.png",
-    code: "https://github.com/Eunji-Elly-Lee/eunji-elly-lee.github.io",
-  }
+      "Hangman game in Java",
+    image: "./assets/hangman.png",
+    code: "https://github.com/Eunji-Elly-Lee/Hangman-In-Java",
+  },
+  {
+    title: "Tic Tac Toe",
+    subtitle: "Java",
+    description:
+      "Tic Tac Toe game in Java",
+    image: "./assets/tictactoe.png",
+    code: "https://github.com/Eunji-Elly-Lee/TicTacToe-In-Java",
+  },
 ];
