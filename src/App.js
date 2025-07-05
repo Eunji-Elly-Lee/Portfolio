a87d74ecc88eb704b070c0165cef715beef86953
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2025-07-05 23:55:30 +0900

    feat: enable Korean/English toggle for all sections

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Projects from "./components/Projects";
@@ -5,13 +6,15 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 
 function App() {
+  const [useKorean, setUseKorean] = useState(true);
+
   return (
     <div className="wrapper">
-      <NavBar />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
+      <NavBar useKorean={useKorean} setUseKorean={setUseKorean} />
+      <About useKorean={useKorean} />
+      <Projects useKorean={useKorean} />
+      <Skills useKorean={useKorean} />
+      <Contact useKorean={useKorean} />
     </div>
   );
 }
